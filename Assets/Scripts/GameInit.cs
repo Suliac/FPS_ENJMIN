26eb306863a72ac85124feaea5daa2255c863449
Suliac Blineau <suliac.blineau@orange.fr>
2017-11-19 12:26:39 +0100

    Add assets

---
+++ Assets/Scripts/GameInit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameInfoHandler.PlayerUi.SetActive(false); // We don't want our UI to appear when we are in the lobby screen
+
+	}
+	
+	
+}
