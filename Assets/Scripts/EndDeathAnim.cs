6634050c071ed61cb901f07761caee6c0e838e1c
Suliac Blineau <suliac.blineau@orange.fr>
2017-12-26 19:06:12 +0100

    Add anim death

---
+++ Assets/Scripts/EndDeathAnim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndDeathAnim : MonoBehaviour {
+
+    LifeBehaviour life;
+
+	// Use this for initialization
+	void Start () {
+        life = GetComponentInParent<LifeBehaviour>();
+	}
+	
+	public void OnEndDeathAnimation()
+    {
+        if(life)
+        {
+            life.OnEndDeathAnim();
+        }
+    }
+}
